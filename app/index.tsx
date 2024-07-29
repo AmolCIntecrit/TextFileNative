3887266d633e15cea4d69c4ce33e51f39abf0baa
Amol Chitale <amolchitaledcjr@gmail.com>
2024-07-29 17:15:39 +0530

    first filesystem and documentpicker implementation

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,32 @@
-import { Text, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
+import { Text, TouchableOpacity, View } from "react-native";
+import * as DocumentPicker from "expo-document-picker";
+import { useEffect, useState } from "react";
+import * as FileSystem from "expo-file-system";
 
 export default function Index() {
+  const [uri, setURI] = useState<string>("");
+  const [data, setData] = useState<string>("");
+
+  const handleClick = async () => {
+    const res: DocumentPicker.DocumentPickerResult =
+      await DocumentPicker.getDocumentAsync({});
+    if (res.assets) {
+      const currURI = res.assets[0].uri;
+      setURI(currURI);
+    }
+  };
+
+  useEffect(() => {
+    const readFile = async () => {
+      const text = await FileSystem.readAsStringAsync(uri);
+      setData(text);
+    };
+    if (uri.length > 0) {
+      readFile();
+    }
+  }, [uri]);
+
   return (
     <View
       style={{
@@ -9,7 +35,17 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <StatusBar style="inverted" />
+
+      {data.length > 0 ? (
+        <Text>{data}</Text>
+      ) : (
+        <View>
+          <TouchableOpacity onPress={handleClick}>
+            <Text>Click here to add file</Text>
+          </TouchableOpacity>
+        </View>
+      )}
     </View>
   );
 }
